edea2d8256e2786a93497790da476ebc9cdd4ef0
HTTTdh <thuthanh0501@gmail.com>
2025-09-09 09:25:06 +0700

    pull

--- Backend/config/database.py
+++ Backend/config/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL : str = "postgresql://minh:123456@localhost:5433/A2A"
+DATABASE_URL : str = "postgresql://postgresql:postgresql@localhost:5433/postgresql"
 
 engine = create_engine(
     url = DATABASE_URL 

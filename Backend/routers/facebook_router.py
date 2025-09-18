57abb80c912d0a948c68c293b7c5a72e8fa219eb
HTTTdh <thuthanh0501@gmail.com>
2025-09-18 12:44:33 +0700

    fix

--- Backend/routers/facebook_router.py
+++ Backend/routers/facebook_router.py
@@ -29,7 +29,7 @@ def delete_page(page_id: int):
 
 FB_CLIENT_ID = "4238615406374117"
 FB_CLIENT_SECRET = "47d60fe20efd7ce023c35380683ba6ef"
-REDIRECT_URI = "https://chatbotbe.haduyson.com/api/auth/facebook/callback"
+REDIRECT_URI = "https://chatbotbe.haduyson.com/facebook-pages/callback"
 
 @router.get("/callback")
 def facebook_callback(code: str):

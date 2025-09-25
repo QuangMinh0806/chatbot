bcbd06cc1b081bdeb532496d5f667b5ec800c263
HTTTdh <thuthanh0501@gmail.com>
2025-09-25 10:08:56 +0700

    xoa bang field config

--- Backend/models/field_config.py
+++ Backend/models/field_config.py
@@ -3,13 +3,13 @@ from datetime import datetime
 from config.database import Base
 
 
-# class FieldConfig(Base):
-#     __tablename__ = "field_config"
+class FieldConfig(Base):
+    __tablename__ = "field_config"
 
-#     id = Column(Integer, primary_key=True, autoincrement=True)
-#     thongtinbatbuoc = Column(JSON, nullable=True, default={})
-#     thongtintuychon = Column(JSON, nullable=True, default={})
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    thongtinbatbuoc = Column(JSON, nullable=True, default={})
+    thongtintuychon = Column(JSON, nullable=True, default={})
 
-#     def __repr__(self):
-#         return f"<FieldConfig(id={self.id}, batbuoc={self.thongtinbatbuoc}, tuychon={self.thongtintuychon})>"
+    def __repr__(self):
+        return f"<FieldConfig(id={self.id}, batbuoc={self.thongtinbatbuoc}, tuychon={self.thongtintuychon})>"
 

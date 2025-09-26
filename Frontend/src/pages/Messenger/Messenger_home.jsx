dfa536034059263e5d444e1d156d2df212442d1f
HTTTdh <thuthanh0501@gmail.com>
2025-09-26 17:21:24 +0700

    merge_v2

--- Frontend/src/pages/Messenger/Messenger_home.jsx
+++ Frontend/src/pages/Messenger/Messenger_home.jsx
@@ -114,7 +114,7 @@ export default function ChatPage() {
                                     <span className="text-white text-lg">💬</span>
                                 </div>
                                 <div>
-                                    <h2 className="text-lg font-semibold text-gray-900">Chat Realtime</h2>
+                                    <h2 className="text-lg font-semibold text-gray-900">Chat với THANHMAIHSK</h2>
                                     <div className="flex items-center gap-2">
                                         <div className={`w-2 h-2 rounded-full ${isConnected ? 'bg-green-500' : 'bg-red-500'}`}></div>
                                         <p className="text-gray-600 text-xs">

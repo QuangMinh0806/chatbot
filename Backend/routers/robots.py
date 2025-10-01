4fa90e85ebb0434463b3979316410a2fca3017a1
HTTTdh <thuthanh0501@gmail.com>
2025-10-01 09:55:03 +0700

    add file robots

---
+++ Backend/routers/robots.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+from fastapi.responses import PlainTextResponse
+
+router = APIRouter()
+
+@router.get("/robots.txt", response_class=PlainTextResponse)
+async def get_robots():
+    return """User-agent: Facebot
+Allow: /
+
+User-agent: facebookexternalhit
+Allow: /
+
+User-agent: *
+Allow: /"""
